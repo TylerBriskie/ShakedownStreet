2045579c0c046f471723d9f140df666a70e71370
Tyler Briskie <tylerbriskie@gmail.com>
2016-06-09 16:08:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+var newGame = function(){
+	var gameState = 0;
+	while (gameState = 0) {
+		prompt playerName = ("Welcome to Shakedown Street.  What's your name, friend?");
+		console.log("Nice to meet you," + playerName + ".")
+		gamestate = 1;
+	}
 
+};
