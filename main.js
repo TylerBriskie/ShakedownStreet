c846629ab27f7d2d3521ebbabb97139a6e74a636
Tyler Briskie <tylerbriskie@gmail.com>
2016-06-15 16:15:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 var newGame = function(){
-	var gameState = 0;
-	while (gameState = 0) {
+	var gameState = "new";
+	while (gameState = "new") {
+		
 		prompt playerName = ("Welcome to Shakedown Street.  What's your name, friend?");
-		console.log("Nice to meet you," + playerName + ".")
-		gamestate = 1;
+		if (playerName = "T-bird"){
+			console.log("No way! My name is T-Bird.  What a wild, crazy coincidence.  Strange we've never met before.")
+		};
+		else {
+		console.log("Nice to meet you," + playerName + ".  My name's T-Bird.")};
+		
+		console.log("You know what they say - never miss a Sunday show.");
+		console.log("Well this is definitely one not to miss, and doors open in an hour.");
+		console.log("We need to find tickets, and fast.  Let's take a walk around the lot");
+		console.log("You have 28 poi")
+		gameState = "running";
 	}
-
+	
 };
