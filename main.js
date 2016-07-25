5855f1bf7e028a15814319e78abc75ee71ba9872
Tyler Briskie <tylerbriskie@gmail.com>
2016-07-25 15:07:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var newGame = function(){
 	var gameState = "new";
-	while (gameState = "new") {
+	if (gameState = "new") {
 		
 		prompt playerName = ("Welcome to Shakedown Street.  What's your name, friend?");
 		if (playerName = "T-bird"){
@@ -8,12 +8,21 @@ var newGame = function(){
 		};
 		else {
 		console.log("Nice to meet you," + playerName + ".  My name's T-Bird.")};
-		
+	
 		console.log("You know what they say - never miss a Sunday show.");
 		console.log("Well this is definitely one not to miss, and doors open in an hour.");
 		console.log("We need to find tickets, and fast.  Let's take a walk around the lot");
 		console.log("You have 28 poi")
-		gameState = "running";
+		gameState = "southGate";
+}
+	
+	else if (gameState = "southGate") {
+		console.log("Here she is, the best hippie gathering on the planet.");
+		console.log("We've got crunchies, ravers, anarchists, stoners, vegans, hyper-vegans.");
+		console.log("Anyone and everyone comes out for Phish.");
+		console.log("Welp, let's find some tickets");
+		prompt destination = ("Where do you want to look for a ticket?");
+		gameState = destination;
 	}
 	
 };
