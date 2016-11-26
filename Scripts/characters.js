8c3bb1b7938d9b7965695dcec1ab5b8925a231ff
Tyler Briskie <tylerbriskie@gmail.com>
2016-11-25 18:21:17 -0700

    working on layout

---
+++ Scripts/characters.js
@@ -0,0 +1,33 @@
+var names = ["Ember", "Echo", "Moon Song", "Energy Turtle", "Lyric", "India", "Wind Song", "August","Rain", "Aspen", "Rainbow Rose", "Jazz", "Fudgy", "Marcus", "Ernesto", "Johnny", "Slim", "Trev", "Adam", "Thomas", "Moon", "Flow", "Frankie", "Applejack"];
+var traits = ["spun", "high", "drunk", "hungover", "tripping", "sober", "hungry", "thirsty"];
+
+
+function Character (name,trait,hasTicket){
+  this.name = names[Math.floor(Math.random()*names.length)];
+  this.trait = traits[Math.floor(Math.random()*traits.length)];
+  this.hasTicket = false;
+}
+
+var wook_0 = new Character();
+var wook_1 = new Character();
+var wook_2 = new Character();
+var wook_3 = new Character();
+var wook_4 = new Character();
+var wook_5 = new Character();
+var wook_6 = new Character();
+var wook_7 = new Character();
+var lotWooks = [wook_0, wook_1, wook_2, wook_3, wook_4, wook_5, wook_6, wook_7];
+whoHasTicket();
+
+
+function randomWook(){
+  var a_wook = '';
+  a_wook = lotWooks[Math.floor(Math.random()*lotWooks.length)]
+  return a_wook;
+};
+
+function whoHasTicket(){
+  var ticketHolder = randomWook();
+  ticketholder.hasTicket = true;
+
+};
---
+++ Scripts/main.js
@@ -0,0 +1,28 @@
+// var newGame = function(){
+// 	var gameState = "new";
+// 	if (gameState = "new") {
+//
+// 		prompt playerName = ("Welcome to Shakedown Street.  What's your name, friend?");
+// 		if (playerName = "T-bird"){
+// 			console.log("No way! My name is T-Bird.  What a wild, crazy coincidence.  Strange we've never met before.")
+// 		};
+// 		else {
+// 		console.log("Nice to meet you," + playerName + ".  My name's T-Bird.")};
+//
+// 		console.log("You know what they say - never miss a Sunday show.");
+// 		console.log("Well this is definitely one not to miss, and doors open in an hour.");
+// 		console.log("We need to find tickets, and fast.  Let's take a walk around the lot");
+// 		console.log("You have 28 poi")
+// 		gameState = "southGate";
+// }
+//
+// 	else if (gameState = "southGate") {
+// 		console.log("Here she is, the best hippie gathering on the planet.");
+// 		console.log("We've got crunchies, ravers, anarchists, stoners, vegans, hyper-vegans.");
+// 		console.log("Anyone and everyone comes out for Phish.");
+// 		console.log("Welp, let's find some tickets");
+// 		prompt destination = ("Where do you want to look for a ticket?");
+// 		gameState = destination;
+// 	}
+//
+// };
